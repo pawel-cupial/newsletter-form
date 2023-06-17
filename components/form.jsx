8830e4fc1b71001df18978fa68fcccd0b8437f0e
Paweł Cupiał <p.cupial@outlook.com>
2023-06-17 19:54:25 +0200

    Added simple form validation

--- components/form.jsx
+++ components/form.jsx
@@ -5,36 +5,55 @@ import { useState } from "react"
 const Form = () => {
     const [emailInputValue, setEmailInputValue] = useState('');
     const [isValid, setIsValid] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
     const [isTouched, setIsTouched] = useState(false);
     
-    const isInvalid = !isValid && isTouched
+    const dataIsInvalid = !isValid && isTouched
+    const dataIsValid = isValid && isTouched
 
     const changeHanlder = e => {
         setEmailInputValue(e.target.value)
     }
     const submitHandler = (e) => {
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        const email = emailInputValue;
         e.preventDefault();
         setIsTouched(true)
 
         if(emailInputValue.trim() === '') {
             setIsValid(false)
+            setErrorMsg('Field cannot be empty');
             return
+        }  else if(!emailRegex.test(email)) {
+            setIsValid(false)
+            setErrorMsg('Invalid e-mail format');
+            return
+        } else {
+            setIsValid(true)
         }
 
         setIsValid(true)
-        console.log(emailInputValue);
         setEmailInputValue('')
     }
 
     return (
         <form method="post" onSubmit={submitHandler}>
-            <p>JO JE FORMULORZ</p>
-            <label htmlFor="email">
-                Email address
-                <input type="text" placeholder="Enter your e-mail" id="email" value={emailInputValue} onChange={changeHanlder} />
-            </label>
-            <button type="submit">Jo je knefel</button>
-            {isInvalid && <p>Field cannot be empty</p>}
+            <div>
+                <p>Stay updated!</p>
+                <p>Join 60,000+ product managers reciving monthly updates on:</p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <label htmlFor="email">
+                    Email address
+                    <input type="text" placeholder="email@company.com" id="email" value={emailInputValue} onChange={changeHanlder} />
+                </label>
+                <button type="submit">Subscribe to monthly newsletter</button>
+                {dataIsInvalid && <p>{errorMsg}</p>}
+                {dataIsValid && <p>your form was successfully submitted</p>}
+            </div>
         </form>
     )
 }
