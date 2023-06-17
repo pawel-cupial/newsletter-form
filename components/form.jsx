5596ab92945dd2752a670b53f7bc67f6f68a9051
Paweł Cupiał <p.cupial@outlook.com>
2023-06-17 21:17:01 +0200

    Added success page & redirect

--- components/form.jsx
+++ components/form.jsx
@@ -1,39 +1,38 @@
 "use client"
 
 import { useState } from "react"
+import { useRouter } from 'next/navigation';
 
 const Form = () => {
     const [emailInputValue, setEmailInputValue] = useState('');
-    const [isValid, setIsValid] = useState(false);
+    const [isValid, setIsValid] = useState();
     const [errorMsg, setErrorMsg] = useState('');
     const [isTouched, setIsTouched] = useState(false);
     
-    const dataIsInvalid = !isValid && isTouched
-    const dataIsValid = isValid && isTouched
+    const router = useRouter();
+
+    const dataIsInvalid = !isValid && isTouched;
 
     const changeHanlder = e => {
-        setEmailInputValue(e.target.value)
+        setEmailInputValue(e.target.value);
     }
     const submitHandler = (e) => {
         const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
         const email = emailInputValue;
         e.preventDefault();
-        setIsTouched(true)
+        setIsTouched(true);
 
         if(emailInputValue.trim() === '') {
-            setIsValid(false)
+            setIsValid(false);
             setErrorMsg('Field cannot be empty');
             return
         }  else if(!emailRegex.test(email)) {
-            setIsValid(false)
+            setIsValid(false);
             setErrorMsg('Invalid e-mail format');
             return
         } else {
-            setIsValid(true)
+            router.push('/success');
         }
-
-        setIsValid(true)
-        setEmailInputValue('')
     }
 
     return (
@@ -52,7 +51,6 @@ const Form = () => {
                 </label>
                 <button type="submit">Subscribe to monthly newsletter</button>
                 {dataIsInvalid && <p>{errorMsg}</p>}
-                {dataIsValid && <p>your form was successfully submitted</p>}
             </div>
         </form>
     )
---
+++ pages/success.jsx
@@ -0,0 +1,7 @@
+const Success = () => {
+    return (
+        <p>UDAŁO SIĘ!</p>
+    )
+}
+
+export default Success
